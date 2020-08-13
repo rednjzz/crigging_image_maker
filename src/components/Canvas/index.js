bdab2460bb76579f6e19d22055db3fec5ea1d49b
rednjzz <rednjzz@gmail.com>
2020-08-13 17:53:27 +0900

    nextCoordnation 값을 적용 하여 이미지 합성

--- src/components/Canvas/index.js
+++ src/components/Canvas/index.js
@@ -4,18 +4,11 @@ import CraneModule from './CraneModule';
 const canvasWidth = 720;
 const canvasHeight = 1280;
 const offSetX = 0;
-const offSetY = 0;
+const offSetY = 100;
 
-// const testData = {
-//   x1: 236,
-//   y1: 60,
-//   x2: 240,
-//   y2: 182,
-//   wX: 236,
-//   wY: 60,
-//   angle: 0,
-//   imgSrc: 'http://localhost:3001/images/1_body.png'
-// };
+let wX = 63;
+let wY = 220;
+
 const modules = [
   {
     x1: 63,
@@ -27,16 +20,26 @@ const modules = [
     angle: 0,
     imgSrc: 'http://localhost:3001/images/1_body.png'
   },
-  // {
-  //   x1: 202,
-  //   y1: 275,
-  //   x2: 200,
-  //   y2: 122,
-  //   wX: 240,
-  //   wY: 182,
-  //   angle:20,
-  //   imgSrc: 'http://localhost:3001/images/22_boom.png'
-  // }
+  {
+    x1: 202,
+    y1: 275,
+    x2: 200,
+    y2: 122,
+    wX: 240,
+    wY: 177,
+    angle:20,
+    imgSrc: 'http://localhost:3001/images/22_boom.png'
+  },
+  {
+    x1: 203,
+    y1: 275,
+    x2: 200,
+    y2: 124,
+    wX: 200,
+    wY: 122,
+    angle:20,
+    imgSrc: 'http://localhost:3001/images/21_boom2.png'
+  }
 ]
 
 function Canvas() {
@@ -58,7 +61,7 @@ function Canvas() {
     //   testData.imgSrc, 
     //   context); 
     // mod.draw(); 
-    
+
     modules.map((data) => {
       const ctx = canvasRef.current.getContext('2d');
       const mod = new CraneModule(
@@ -66,8 +69,8 @@ function Canvas() {
         data.y1,
         data.x2,
         data.y2,
-        data.wX, 
-        data.wY,
+        wX,
+        wY,
         offSetX,
         offSetY,
         data.angle,
@@ -77,6 +80,8 @@ function Canvas() {
         ctx); 
       mod.draw();
       mod.drawPoints();
+      wX = mod.nextCoordX;
+      wY = mod.nextCoordY;
       return mod;
     })  
   }, [])
